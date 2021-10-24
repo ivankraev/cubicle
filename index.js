67a34cca1192b0b32e25140195ff62273c8b2a8b
Ivan Kraev <ivankraev2@abv.bg>
2021-10-24 03:11:21 +0300

    some configurations

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 
 const config = require('./config/config.js')
-
+const handlebars = require('express-handlebars')
 const app = express();
 
-app.listen(config.PORT)
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(config.PORT);
