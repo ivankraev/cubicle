0b5ede3e737b86e36c4387c0f8ad80c840260ae6
Ivan Kraev <ivankraev2@abv.bg>
2021-10-26 01:52:19 +0300

    upd

--- router.js
+++ router.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 const productController = require('./controllers/productController')
-
+const aboutController = require('./controllers/aboutController')
 
 router.use(productController)
+router.use('/about', aboutController)
 
 
 module.exports = router
