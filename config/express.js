043cdd033072d65307128b8a2fa8b21bec7d2c01
Ivan Kraev <ivankraev2@abv.bg>
2021-10-26 04:06:13 +0300

    update

--- config/express.js
+++ config/express.js
@@ -7,5 +7,6 @@ function setupExpress(app) {
     }))
     app.set('view engine', 'hbs');
     app.use(express.static('public'))
+    app.use(express.urlencoded())
 }
 module.exports = setupExpress;
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
-
+const Cube = require('../models/Cube')
 const router = Router();
-
+const uniqid = require('uniqid')
 
 router.get('/', (req, res) => {
     res.render('home', { title: 'Browse' });
@@ -9,6 +9,23 @@ router.get('/', (req, res) => {
 
 router.get('/create', (req, res) => {
     res.render('create', { title: 'Create' });
+})
+
+router.post('/create', (req, res) => {
+    let data = req.body;
+    let cube = new Cube(uniqid(),
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficultyLevel)
+    console.log(cube);
+
+    res.redirect('/')
+})
+
+
+router.get('/details/:id', (req, res) => {
+    res.render('details', { title: 'Details' });
 })
 
 module.exports = router
