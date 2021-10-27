abf1d8dcf5066429ff8d623f7a65d8aa057fc135
Ivan Kraev <ivankraev2@abv.bg>
2021-10-27 04:29:54 +0300

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,7 @@ router.get('/create', (req, res) => {
     res.render('create', { title: 'Create' });
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', validateProduct, (req, res) => {
     let data = req.body;
     productService.create(data)
     res.redirect('/')
@@ -25,5 +25,22 @@ router.post('/create', (req, res) => {
 router.get('/details/:id', (req, res) => {
     res.render('details', { title: 'Details', cube: productService.getOne(req.params.id) });
 })
+
+function validateProduct(req, res, next) {
+    let isValid = true;
+    if ((req.body.name.trim().length < 2) || !req.body.imageUrl) {
+        isValid = false;
+    }
+    if (isValid) {
+        next();
+    } else {
+        res.redirect('/create')
+    }
+
+
+
+}
+
+
 
 module.exports = router
